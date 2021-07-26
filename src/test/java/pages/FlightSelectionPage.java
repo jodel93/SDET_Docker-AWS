cb40e214b773c6fed27a939e2a9be7de11e99958
Jorge Delgado <jorgedelgadopernia@gmail.com>
2021-07-26 12:59:43 -0300

    Modify Flights and FlightSelection PO

--- src/test/java/pages/FlightSelectionPage.java
+++ src/test/java/pages/FlightSelectionPage.java
@@ -1,2 +1,33 @@
-package pages;public class FlightSelectionPage {
+package pages;
+
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FlightSelectionPage extends BasePage {
+
+    @FindBy(name = "reserveFlights")
+    private WebElement firstSubmitButton;
+
+    @FindBy(name ="buyFlights")
+    private WebElement secondSubmitButton;
+
+    @Autowired
+    public FlightSelectionPage(WebDriver driver, WebDriverWait wait){
+        super(driver, wait);
+    }
+
+    public void goToFlightDetailsPage(){
+        getWait().until(ExpectedConditions.elementToBeClickable(firstSubmitButton));
+        firstSubmitButton.click();
+        getWait().until(ExpectedConditions.elementToBeClickable(secondSubmitButton));
+        secondSubmitButton.click();
+    }
+
 }
--- src/test/java/pages/FlightsPage.java
+++ src/test/java/pages/FlightsPage.java
@@ -1,2 +1,37 @@
-package pages;public class FlightsPage {
+package pages;
+
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FlightsPage extends BasePage {
+
+    @FindBy(name ="passCount")
+    private WebElement passengerSelector;
+
+    @FindBy(name="findFlights")
+    private WebElement submitButton;
+
+    @Autowired
+    public FlightsPage(WebDriver driver, WebDriverWait wait){
+        super(driver, wait);
+    }
+
+    public void selectNumberOfPassengers(int numberOfPassengers){
+        getWait().until(ExpectedConditions.visibilityOf(passengerSelector));
+        Select select = new Select(passengerSelector);
+        select.selectByValue(String.valueOf(numberOfPassengers));
+    }
+
+    public void goToFlightFindPage(){
+        submitButton.click();
+    }
+
 }

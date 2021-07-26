e9cbaaa18f02ae09900fead138a873bc6f094881
Jorge Delgado <jorgedelgadopernia@gmail.com>
2021-07-26 17:38:07 -0300

    Add support to diferent browsers

--- src/test/java/configuration/Driver.java
+++ src/test/java/configuration/Driver.java
@@ -1,21 +1,27 @@
 package configuration;
 
+import enums.Browser;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import utils.factory.DriverFactory;
 
 @Configuration
 @ComponentScan(basePackages = {"pages", "base", "tests"})
+@PropertySource("classpath:/properties/driver.properties")
 public class Driver {
+
+    @Value("${browser.type}")
+    private Browser browserType;
 
     @Bean
     public WebDriver webDriver(){
-        System.setProperty("webdriver.chrome.driver",
-                System.getProperty("user.dir") + "/src/test/resources/driver/chromedriver.exe");
-        return new ChromeDriver();
+        return DriverFactory.setUpDriver(browserType);
     }
 
     @Bean
---
+++ src/test/java/utils/factory/DriverFactory.java
@@ -0,0 +1,33 @@
+package utils.factory;
+
+import enums.Browser;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+
+    private static String DRIVER_PATH = System.getProperty("user.dir") + "/src/test/resources/driver/%s";
+    private static WebDriver driver;
+
+    public static WebDriver setUpDriver(Browser browser){
+        if(Browser.chrome == browser){
+            System.setProperty("webdriver.chrome.driver",
+                    String.format(DRIVER_PATH, Browser.chrome.label));
+            driver = new ChromeDriver();
+        }
+        if(Browser.firefox == browser){
+            System.setProperty("webdriver.gecko.driver",
+                    String.format(DRIVER_PATH, Browser.firefox.label));
+            driver = new FirefoxDriver();
+        }
+        if(Browser.ie == browser){
+            //For IE
+        }
+
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+}

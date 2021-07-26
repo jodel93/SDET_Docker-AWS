2458b82fb9af09520b76394e3264c19675de81bc
Jorge Delgado <jorgedelgadopernia@gmail.com>
2021-07-26 12:21:01 -0300

    Modify BasePage

--- src/test/java/base/BasePage.java
+++ src/test/java/base/BasePage.java
@@ -1,17 +1,25 @@
 package base;
 
+import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Getter
+@Component
 public class BasePage {
 
-    private WebDriver driver;
-    private WebDriverWait wait;
+    private final WebDriver driver;
+    private final WebDriverWait wait;
 
+    @Autowired
     public BasePage(WebDriver driver, WebDriverWait wait){
         this.driver = driver;
         this.wait = wait;
         PageFactory.initElements(this.driver, this);
     }
+
+    protected void navigateTo(String url){ driver.get(url);}
 }
